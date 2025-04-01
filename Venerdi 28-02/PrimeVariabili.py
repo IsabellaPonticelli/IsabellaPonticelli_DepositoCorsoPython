e8c363abf1749eb8f204c5804fdfe2d433d98247
Isabella Ponticelli <ponticelliisabella@gmail.com>
2025-04-01 14:41:40 +0200

    Add files via upload

---
+++ Venerdi 28-02/PrimeVariabili.py
@@ -0,0 +1,33 @@
+nome= "Alice"
+età = 25
+print("Il mio nome è", nome, "e ho", età, "anni.")
+
+#numeri int cioè non decimali 1 e -1
+# numeri float con virgola mobile(punto) = 1.0 oppure -1.0
+ 
+nome = 'Alice'
+msg= "Ciao!"
+
+s= "Python"
+print(s[0])
+print(s[2])
+#print(s[100]) #errore out of range
+
+print(msg + " " + nome + ", Benvenuta!")
+
+w = "Ciao, mondo!"
+print(len(w))
+print(w.upper())
+print(w.split(','))
+print(w.replace('mondo', 'universo'))
+
+x = 5
+y= 10
+z= 7
+print(x>y and y>z)
+print(x>y or z>y)
+print(not(x>y))
+
+print(x == y)
+print( x != y)
+print(x < y)
---
+++ Venerdi 28-02/PrimiImput.py
@@ -0,0 +1,3 @@
+nome= input("Inserisci il tuo nome: ")
+età= int(input("Inserisci la tua età: "))
+print("Ciao, " + nome+ "! Benvenuto in Python!")
